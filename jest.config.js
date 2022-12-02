15eb5276c5a03398b65af5b5c47930d64e0dbdc8
abdurRahmanCorel <111302538+abdurRahmanCorel@users.noreply.github.com>
2022-12-02 01:45:11 -0800

    CD, Test and test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  moduleNameMapper: {
-    '\\.(scss|sass|css)$': 'identity-obj-proxy',
-  },
-  collectCoverageFrom: [
-    '**/*.{js,jsx,ts,tsx}',
-    '!**/.next/**',
-    '!**/node_modules/**',
-    '!**/coverage/**',
-    '!**/public/**',
-    "!**.config.**",
-    "!**.d.ts**"
-  ],
-};
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+    moduleNameMapper: {
+        '\\.(scss|sass|css)$': 'identity-obj-proxy',
+    },
+    collectCoverageFrom: [
+        '**/*.{js,jsx,ts,tsx}',
+        '!**/.next/**',
+        '!**/node_modules/**',
+        '!**/coverage/**',
+        '!**/public/**',
+        '!**.config.**',
+        '!**.d.ts**',
+    ],
+}
